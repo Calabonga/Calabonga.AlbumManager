c2a458036cd888ad9f391f203519804f88376820
Calabonga <dev@calabonga.net>
2023-11-11 10:58:18 +0700

    try with interfaces

--- src/Calabonga.AlbumManager/Calabonga.AlbumManager.ConsoleApp/Program.cs
+++ src/Calabonga.AlbumManager/Calabonga.AlbumManager.ConsoleApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Calabonga.AlbumsManager;
 using Calabonga.AlbumsManager.Creators.Folder;
+//builder.AddCreator<FolderTreeAlbumCreator, FolderTreeAlbumCreatorConfiguration>(x => x.SourceRootPath = "C:\\Projects\\Calabonga.AlbumManager\\whatnot\\Gallery");
 
-var builder = new AlbumManagerBuilder();
-
-builder.AddCreator<FolderAlbumCreator, FolderAlbumCreatorConfiguration>(x => x.SourcePath = "C:\\Projects\\Calabonga.AlbumManager\\whatnot\\Animals");
-// builder.AddCreator<FolderTreeAlbumCreator, FolderTreeAlbumCreatorConfiguration>(x => x.SourceRootPath = "C:\\Projects\\Calabonga.AlbumManager\\whatnot\\Gallery");
+var builder = new AlbumManagerBuilder()
+    .AddCreator<FolderAlbumCreator, FolderAlbumCreatorConfiguration>(x => x.SourcePath = "C:\\Projects\\Calabonga.AlbumManager\\whatnot\\Animals")
+    .AddViewer<FolderAlbumViewerConfiguration>(x => x.TakeTop = 10)
+    .AddMetadataReader<FolderAlbumMetadataReaderConfiguration>(x => x.Enabled = false)
+    .AddEditor<FolderAlbumEditorConfiguration>(x => x.Enabled = false)
+    .AddUploader<FolderAlbumUploaderConfiguration>(x => x.Enabled = false);
 
 var manager = builder.Build();
 
@@ -15,4 +18,24 @@ var view = manager.GetView();
 foreach (var item in view.Items)
 {
     Console.WriteLine($"{item.FileSize} - {item.FileName}");
+}
+
+public class FolderAlbumViewerConfiguration
+{
+    public int TakeTop { get; set; }
+}
+
+public class FolderAlbumMetadataReaderConfiguration
+{
+    public bool Enabled { get; set; }
+}
+
+public class FolderAlbumEditorConfiguration
+{
+    public bool Enabled { get; set; }
+}
+
+public class FolderAlbumUploaderConfiguration
+{
+    public bool Enabled { get; set; }
 }
--- src/Calabonga.AlbumManager/Calabonga.AlbumManager/AlbumManagerBuilder.cs
+++ src/Calabonga.AlbumManager/Calabonga.AlbumManager/AlbumManagerBuilder.cs
@@ -4,17 +4,44 @@ namespace Calabonga.AlbumsManager;
 /// <summary>
 /// Configuration builder for <see cref="AlbumManager"/>
 /// </summary>
-public class AlbumManagerBuilder
+public class AlbumManagerBuilder : IAlbumManagerBuilder
 {
     private ICreator _creator = null!;
 
-    public void AddCreator<TCreator, TConfiguration>(Action<TConfiguration> configuration)
-        where TCreator : AlbumCreatorBase<TConfiguration>
-        where TConfiguration : class, new()
+    public IAlbumManagerBuilder AddCreator<TCreator, TCreatorConfiguration>(Action<TCreatorConfiguration> configuration)
+        where TCreator : AlbumCreatorBase<TCreatorConfiguration>
+        where TCreatorConfiguration : class, new()
     {
-        var config = new TConfiguration();
+        var config = new TCreatorConfiguration();
         configuration(config);
-        _creator = (AlbumCreatorBase<TConfiguration>)Activator.CreateInstance(typeof(TCreator), config)!;
+        _creator = (AlbumCreatorBase<TCreatorConfiguration>)Activator.CreateInstance(typeof(TCreator), config)!;
+
+        return this;
+    }
+
+    IAlbumManagerBuilder IAlbumManagerBuilder.AddCreator<TCreator, TCreatorConfiguration>(Action<TCreatorConfiguration> configuration)
+    {
+        return this;
+    }
+
+    public IAlbumManagerBuilder AddViewer<TViewerConfiguration>(Action<TViewerConfiguration> configuration)
+    {
+        return this;
+    }
+
+    public IAlbumManagerBuilder AddMetadataReader<TMetadataReaderConfiguration>(Action<TMetadataReaderConfiguration> configuration)
+    {
+        return this;
+    }
+
+    public IAlbumManagerBuilder AddEditor<TEditorConfiguration>(Action<TEditorConfiguration> configuration)
+    {
+        return this;
+    }
+
+    public IAlbumManagerBuilder AddUploader<TUploaderConfiguration>(Action<TUploaderConfiguration> configuration)
+    {
+        return this;
     }
 
     public AlbumManager Build()
@@ -23,4 +50,19 @@ public class AlbumManagerBuilder
         var manager = new AlbumManager(items);
         return manager;
     }
+}
+
+public interface IAlbumManagerBuilder
+{
+    IAlbumManagerBuilder AddCreator<TCreator, TCreatorConfiguration>(Action<TCreatorConfiguration> configuration);
+
+    IAlbumManagerBuilder AddViewer<TViewerConfiguration>(Action<TViewerConfiguration> configuration);
+
+    IAlbumManagerBuilder AddMetadataReader<TMetadataReaderConfiguration>(Action<TMetadataReaderConfiguration> configuration);
+
+    IAlbumManagerBuilder AddEditor<TEditorConfiguration>(Action<TEditorConfiguration> configuration);
+
+    IAlbumManagerBuilder AddUploader<TUploaderConfiguration>(Action<TUploaderConfiguration> configuration);
+
+    AlbumManager Build();
 }
